0c45236305fa55ecc1276219052aa33c61b85065
Amith B V <amithrao0606@gmail.com>
2025-07-05 18:40:52 +0530

    Created functions which handles the user's signin and signup. And made it available to all screens through "AuthContextProvider".

--- context/auth-context.tsx
+++ context/auth-context.tsx
@@ -1,8 +1,8 @@
-import React, { createContext } from "react";
-import { Models } from "react-native-appwrite";
+import React, { createContext, useContext } from "react";
+import { authFunc } from "../lib/AuthFunctions";
 
 type AuthContextType = {
-  user: Models.User<Models.Preferences> | null;
+  //   user: Models.User<Models.Preferences> | null;
   signUp: (email: string, password: string) => Promise<string | null>;
   signIn: (email: string, password: string) => Promise<string | null>;
 };
@@ -14,11 +14,20 @@ export function AuthContextProvider({
 }: {
   children: React.ReactNode;
 }) {
+  const { signUp, signIn } = authFunc;
+
   return (
-    <AuthContext.Provider value={{ user, signIn, signUp }}>
+    <AuthContext.Provider value={{ signIn, signUp }}>
       {children}
     </AuthContext.Provider>
   );
 }
 
-export function useAuth() {}
+export function useAuth() {
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error("'useAuth' must be inside of the 'AuthContextProvider'!!");
+  }
+
+  return context;
+}
