73cb45bc9b94f9e2b33a9398dc56937940cb6a18
Amith B V <amithrao0606@gmail.com>
2025-07-05 08:01:26 +0530

    Implemented basic RouteGuard function.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,29 @@
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
+import { useEffect } from "react";
+
+function RouteGuard({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+  const isAuth = false;
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (!isAuth) {
+        router.replace("/auth");
+      } else {
+        router.replace("/");
+      }
+    }, 1000);
+  });
+
+  return <>{children}</>;
+}
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <RouteGuard>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </RouteGuard>
   );
 }
