cf08899f7215b7c4b2b80bea5d433a34277f69f3
Amith B V <amithrao0606@gmail.com>
2025-07-06 17:39:46 +0530

    Created the UI for Add-Habit Screen.

--- app/(tabs)/add-habit.tsx
+++ app/(tabs)/add-habit.tsx
@@ -1,9 +1,43 @@
-import { StyleSheet, Text, View } from "react-native";
+import { FREQUENCIES, FrequencyType } from "@/constants";
+import { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { Button, SegmentedButtons, TextInput } from "react-native-paper";
 
 export default function AddHabitScreen() {
+  const [title, setTitle] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+  const [frequency, setFrequency] = useState<FrequencyType>("daily");
+
   return (
     <View style={styles.container}>
-      <Text>Form for adding Habits!!</Text>
+      <TextInput
+        label={"Title"}
+        mode="outlined"
+        style={styles.input}
+        onChangeText={setTitle}
+      />
+
+      <TextInput
+        label={"Description"}
+        mode="outlined"
+        style={styles.input}
+        onChangeText={setDescription}
+      />
+
+      <View style={styles.frequencyContainer}>
+        <SegmentedButtons
+          buttons={FREQUENCIES.map((freq) => ({
+            value: freq,
+            label: freq.charAt(0).toUpperCase() + freq.slice(1),
+          }))}
+          onValueChange={(value) => setFrequency(value as FrequencyType)}
+          value={frequency}
+        />
+      </View>
+
+      <Button mode="contained" disabled={!title || !description}>
+        Add Habit
+      </Button>
     </View>
   );
 }
@@ -11,7 +45,15 @@ export default function AddHabitScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 15,
+    backgroundColor: "#f5f5f5",
     justifyContent: "center",
-    alignItems: "center",
+    marginTop: -50,
+  },
+  input: {
+    marginBottom: 16,
+  },
+  frequencyContainer: {
+    marginBottom: 24,
   },
 });
---
+++ constants/index.ts
@@ -0,0 +1,3 @@
+export const FREQUENCIES = ["daily", "weekly", "monthly"];
+
+export type FrequencyType = (typeof FREQUENCIES)[number];
