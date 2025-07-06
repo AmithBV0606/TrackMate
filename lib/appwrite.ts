19f038726386e82ddf34d1692891a68d2ade8276
Amith B V <amithrao0606@gmail.com>
2025-07-06 17:10:04 +0530

    Created 'habits-db' collection and 'habits' attribute i.e table inside appwrite. Then saved the credentials in '.env' and created a new database instance.

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,6 +1,6 @@
 // This is the config file for connecting appwrite to our project
 
-import { Account, Client } from "react-native-appwrite";
+import { Account, Client, Databases } from "react-native-appwrite";
 
 export const client = new Client()
   .setEndpoint(process.env.EXPO_PUBLIC_APPWRITE_API_ENDPOINT!)
@@ -8,3 +8,7 @@ export const client = new Client()
   .setPlatform(process.env.EXPO_PUBLIC_APPWRITE_PLATFORM!);
 
 export const account = new Account(client);
+export const databases = new Databases(client);
+
+export const DATABASE_ID = process.env.EXPO_PUBLIC_DB_ID!;
+export const HABITS_COLLECTION_ID = process.env.EXPO_PUBLIC_HABITS_COLLECTION_ID!;
