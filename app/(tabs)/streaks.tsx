dcc21289e1eb6f99fea5c3acb6531465d30e45a1
Amith B V <amithrao0606@gmail.com>
2025-07-07 21:39:40 +0530

    Coppied "fetchHabits", "fetchTodaysCompletions" and useEffect from "index.tsx" file.

--- app/(tabs)/streaks.tsx
+++ app/(tabs)/streaks.tsx
@@ -1,6 +1,60 @@
+import { useAuth } from "@/context/auth-context";
+import {
+  COMPLETION_COLLECTION_ID,
+  DATABASE_ID,
+  databases,
+  HABITS_COLLECTION_ID,
+} from "@/lib/appwrite";
+import { Habit, HabitCompletion } from "@/types";
+import { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { Query } from "react-native-appwrite";
 
 export default function StreaksScreen() {
+  const [habits, setHabits] = useState<Habit[]>();
+  const [completedHabits, setCompletedHabits] = useState<HabitCompletion[]>();
+
+  const { user } = useAuth();
+
+  // Fetch habits from the "habits" collections :
+  const fetchHabits = async () => {
+    try {
+      const response = await databases.listDocuments(
+        DATABASE_ID,
+        HABITS_COLLECTION_ID,
+        [Query.equal("user_id", user?.$id ?? "")]
+      );
+
+      // console.log(response);
+      setHabits(response.documents as Habit[]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // Fetch habits from "habit_completion" collections :
+  const fetchCompletions = async () => {
+    try {
+      const response = await databases.listDocuments(
+        DATABASE_ID,
+        COMPLETION_COLLECTION_ID,
+        [Query.equal("user_id", user?.$id ?? "")]
+      );
+
+      const completions = response.documents as HabitCompletion[];
+      setCompletedHabits(completions);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    if (user) {
+      fetchHabits();
+      fetchCompletions();
+    }
+  }, [user]);
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Streaks</Text>
