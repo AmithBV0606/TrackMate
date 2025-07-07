21707ec795ea1a674403eedf38c1b2193f7658d6
Amith B V <amithrao0606@gmail.com>
2025-07-07 06:48:44 +0530

    Created the UI for "Today's Habit" screen and fetched the habits from the database and rendered habit cards.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,22 +1,89 @@
 import { useAuth } from "@/context/auth-context";
-import { StyleSheet, Text, View } from "react-native";
-import { Button } from "react-native-paper";
+import { DATABASE_ID, databases, HABITS_COLLECTION_ID } from "@/lib/appwrite";
+import { Habit } from "@/types";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { useEffect, useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { Query } from "react-native-appwrite";
+import { Button, Surface, Text } from "react-native-paper";
 
 export default function HomeScreen() {
-  const { signOut } = useAuth();
+  const [habits, setHabits] = useState<Habit[]>();
+
+  const { user, signOut } = useAuth();
+
+  const fetchHabits = async () => {
+    try {
+      const response = await databases.listDocuments(
+        DATABASE_ID,
+        HABITS_COLLECTION_ID,
+        [Query.equal("user_id", user?.$id ?? "")]
+      );
+
+      // console.log(response);
+      setHabits(response.documents as Habit[]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchHabits();
+  }, [habits]);
 
   return (
     <View style={styles.container}>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      {/* Header : */}
+      <View style={styles.header}>
+        <Text variant="headlineSmall" style={styles.title}>
+          Today&apos;s Habit
+        </Text>
 
-      <Button
-        mode="contained"
-        onPress={signOut}
-        icon={"logout"}
-        style={{ marginTop: 10 }}
-      >
-        Sign Out
-      </Button>
+        <Button mode="text" onPress={signOut} icon={"logout"}>
+          Sign Out
+        </Button>
+      </View>
+
+      {/* To display habits : */}
+      {habits?.length === 0 ? (
+        <View style={styles.emptyState}>
+          <Text style={styles.emptyStateText}>
+            No Habits yet. Add your first Habit!!
+          </Text>
+        </View>
+      ) : (
+        habits?.map((habit, key) => (
+          <Surface style={styles.card} elevation={0} key={key}>
+            <View style={styles.cardContent}>
+              <Text style={styles.cardTitle}>{habit.title}</Text>
+              <Text style={styles.cardDescription}>{habit.description}</Text>
+
+              <View style={styles.cardFooter}>
+                {/* Streaks : */}
+                <View style={styles.streakBadge}>
+                  <MaterialCommunityIcons
+                    name="fire"
+                    size={18}
+                    color={"#ff9800"}
+                  />
+
+                  <Text style={styles.streakText}>
+                    {habit.streak_count} days streak
+                  </Text>
+                </View>
+
+                {/* Frequency : */}
+                <View style={styles.frequencyBadge}>
+                  <Text style={styles.frequencyText}>
+                    {habit.frequency.charAt(0).toUpperCase() +
+                      habit.frequency.slice(1)}
+                  </Text>
+                </View>
+              </View>
+            </View>
+          </Surface>
+        ))
+      )}
     </View>
   );
 }
@@ -24,12 +91,80 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 16,
+    backgroundColor: "#f5f5f5",
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignContent: "center",
+    marginBottom: 24,
+  },
+  title: {
+    fontWeight: "bold",
+  },
+  card: {
+    marginBottom: 18,
+    borderRadius: 18,
+    backgroundColor: "#f7f2fa",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.08,
+    shadowRadius: 8,
+    elevation: 4,
+  },
+  cardContent: {
+    padding: 20,
+  },
+  cardTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 4,
+    color: "#22223b",
+  },
+  cardDescription: {
+    fontSize: 15,
+    marginBottom: 16,
+    color: "#6c6c80",
+  },
+  cardFooter: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  streakBadge: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#fff3e0",
+    borderRadius: 12,
+    paddingHorizontal: 10,
+    paddingVertical: 4,
+  },
+  streakText: {
+    marginLeft: 6,
+    color: "#ff9800",
+    fontWeight: "bold",
+    fontSize: 14,
+  },
+  frequencyBadge: {
+    backgroundColor: "#ede7f6",
+    borderRadius: 12,
+    paddingHorizontal: 12,
+    paddingVertical: 4,
+  },
+  frequencyText: {
+    color: "#7c4dff",
+    fontWeight: "bold",
+    fontSize: 14,
+  },
+  emptyState: {
+    flex: 1,
     justifyContent: "center",
     alignItems: "center",
   },
-  link: {
-    marginTop: 10,
-    color: "#28A1CC",
-    textDecorationLine: "underline",
+  emptyStateText: {
+    color: "#666666",
+    fontSize: 16,
+    fontWeight: "bold",
   },
 });
--- types/index.ts
+++ types/index.ts
@@ -10,3 +10,13 @@ export type AuthContextType = {
 };
 
 export type FrequencyType = (typeof FREQUENCIES)[number];
+
+export interface Habit extends Models.Document {
+  user_id: string;
+  title: string;
+  description: string;
+  streak_count: number;
+  last_completed: string;
+  frequency: string;
+  created_at: string;
+}
