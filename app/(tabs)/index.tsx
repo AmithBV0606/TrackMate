b14c2e52b1aa9576c2dc78a9eede1f48b6ae704c
Amith B V <amithrao0606@gmail.com>
2025-07-08 08:14:50 +0530

    UI Changes.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -290,7 +290,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    marginTop: "70%",
+    marginTop: "80%",
   },
   emptyStateText: {
     color: "#666666",
